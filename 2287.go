1c5060bdf5f91f715bdfe9b085a6145bb1d6a714
T-c33 <tyechen33@gmail.com>
2023-01-13 20:39:47 +0800

    update

---
+++ 2287.go
@@ -0,0 +1,26 @@
+func rearrangeCharacters(s, target string) int {
+	var cntS, cntT [26]int
+	for _, c := range s {
+		cntS[c-'a']++
+	}
+	for _, c := range target {
+		cntT[c-'a']++
+	}
+	ans := len(s)
+	for i, c := range cntT {
+		if c > 0 {
+			ans = min(ans, cntS[i]/c)
+			if ans == 0 {
+				return 0
+			}
+		}
+	}
+	return ans
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
