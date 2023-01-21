512f6444ea542760e6bf79d66814de481548220e
T-c33 <34746538+T-c33@users.noreply.github.com>
2023-01-21 10:26:14 +0800

    update

---
+++ 1824.go
@@ -0,0 +1,31 @@
+package main
+
+import "math"
+
+func minSideJumps(obstacles []int) int {
+	d := [3]int{1, 0, 1}
+	for _, x := range obstacles[1:] {
+		minCnt := math.MaxInt64 / 2
+		for j := 0; j < 3; j++ {
+			if j == x-1 {
+				d[j] = math.MaxInt64 / 2
+			} else {
+				minCnt = min(minCnt, d[j])
+			}
+		}
+		for j := 0; j < 3; j++ {
+			if j != x-1 {
+				d[j] = min(d[j], minCnt+1)
+			}
+		}
+	}
+	return min(min(d[0], d[1]), d[2])
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/T-c33/leetcode_everyday
 
 go 1.14
-
-require golang.org/x/exp v0.0.0-20230118134722-a68e582fa157 // indirect
