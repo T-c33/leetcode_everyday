23f5e66bd515af93705f1d5f2498e91bfd94ac4f
T-c33 <tyechen33@gmail.com>
2023-01-26 13:03:16 +0800

    update

---
+++ 1663.go
@@ -0,0 +1,18 @@
+package main
+
+func getSmallestString(n, k int) string {
+	s := []byte{}
+	for i := 1; i <= n; i++ {
+		lower := max(1, k-(n-i)*26)
+		k -= lower
+		s = append(s, 'a'+byte(lower)-1)
+	}
+	return string(s)
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
