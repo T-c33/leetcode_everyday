211396a50033aac3baecc35a42460b5230336626
T-c33 <tyechen33@gmail.com>
2023-01-28 22:59:04 +0800

    update

--- 1664.go
+++ 1664.go
@@ -3,14 +3,14 @@ package main
 func waysToMakeFair(nums []int) (res int) {
 	var odd1, even1, odd2, even2 int
 	for i, num := range nums {
-		if i%2 > 0 {
+		if i&1 > 0 {
 			odd2 += num
 		} else {
 			even2 += num
 		}
 	}
 	for i, num := range nums {
-		if i%2 > 0 {
+		if i&1 > 0 {
 			odd2 -= num
 		} else {
 			even2 -= num
@@ -18,11 +18,11 @@ func waysToMakeFair(nums []int) (res int) {
 		if odd1+even2 == odd2+even1 {
 			res++
 		}
-		if i%2 > 0 {
+		if i&1 > 0 {
 			odd1 += num
 		} else {
 			even1 += num
 		}
 	}
-	return res
+	return
 }
